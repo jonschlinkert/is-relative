186c80b369cdd449b40abf0a1fb06e4a012b1cd2
tunnckoCore <mamemto_100@mail.bg>
2014-11-12 19:23:59 +0200

    add tests commented on issue

--- test.js
+++ test.js
@@ -8,18 +8,21 @@
 'use strict';
 
 var assert = require('assert');
-var isRelative = require('../');
+var isRelative = require('./');
 
 describe('isRelative', function() {
   it('should return true if the path appears to be relative', function() {
     assert(isRelative('test/fixtures'));
     assert(isRelative('test/fixtures/'));
     assert(isRelative('test/fixtures/foo.txt'));
+    assert(isRelative('./test/fixtures/foo.txt'));
   });
 
   it('should return false if the path appears to be absolute', function() {
+    assert(isRelative('./test/fixtures/foo.txt'));
     assert(!isRelative('/test/fixtures'));
     assert(!isRelative('/test/fixtures/'));
+    assert(!isRelative('/test/fixtures/baz.md'));
     assert(!isRelative('e://test/fixtures/'));
     assert(!isRelative('e:/test/fixtures/'));
     assert(!isRelative('\\test\\fixtures\\'));
